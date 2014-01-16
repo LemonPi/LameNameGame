e5c0e067cdbe62ae22a85d7083c6a972ad0808d3
LemonPi <johnson9510@hotmail.com>
2014-01-16 14:39:45 -0500

    Added comments

--- list_generate.c
+++ list_generate.c
@@ -5,10 +5,10 @@
 int main(int argc, char** argv) {
     char* user_list = argv[1];
     FILE* infile = fopen(user_list, "r");
-    char line[257];
-    char* work_str;
-    char*** master_storage = malloc(100000);
-    int i = 0, j = 0;   // Counters
+    char line[257];  // max of 256 char per line, 1 for null terminator
+    char* work_str;  // p* used to hold temporary split strings
+    char*** master_storage = malloc(100000);  // value is arbitrary
+    int i = 0, j = 0;   // Counters, max j expected to be constant for each line
 
     // Individual attributes
     char id[] = "initialized";
@@ -17,21 +17,21 @@ int main(int argc, char** argv) {
     int user_number = -666;
 
     for (; fgets(line, 256, infile) != NULL; i++) {
-        printf("%s\n", line);
+        // printf("%s\n", line);
         char* new_line = strchr(line, '\n');
-        if (new_line)
-            *new_line = '\0';
-        work_str = strtok(line, " ,;:/");
-        master_storage[i] = malloc(256 * sizeof(char*));
-        if (NULL == work_str)
-            printf("work_str is null\n");
+        if (new_line)  
+            *new_line = '\0';  // removes newline at end
+        work_str = strtok(line, " ,;:/");  // splits string into tokens based on " ,;:/" char
+        master_storage[i] = malloc(256 * sizeof(char*));  // memory leaky but whatever
+        // if (NULL == work_str)
+            // printf("work_str is null\n");
 
         for (j = 0; work_str != NULL; j++) {
-            char* new_str = malloc(strlen(work_str)+1);
+            char* new_str = malloc(strlen(work_str)+1);  // temporary hoding string
             strcpy(new_str, work_str);
             master_storage[i][j] = new_str;
             printf("[Set %d, %d to %s]\n", i, j, work_str);
-            work_str = strtok(NULL, " ,;:/");
+            work_str = strtok(NULL, " ,;:/");  // strtok recycles memory, old overrides new
         }
     }
 
