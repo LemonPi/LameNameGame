2655aec3f5f68e5c7b640f2b362f036a8e5e467d
LemonPi <johnson9510@hotmail.com>
2014-01-16 22:41:22 -0500

    Restart

--- list_generate.c
+++ list_generate.c
@@ -4,7 +4,13 @@
 #include <stdlib.h>
 
 char* old_s;  // each time keep same pointer
+int points = 0;
 
+typedef struct name_data {
+    char* utorid;
+    char* names[10];
+} name_data;
+    
 char* strsplit(char* s, const char* delim) {
     char *piece;  // each time use new piece
 
@@ -28,11 +34,10 @@ char* strsplit(char* s, const char* delim) {
 }
 
 
-int main(int argc, char** argv) {
-    char* user_list = argv[1];
-    FILE* infile = fopen(user_list, "r");
+void initialize(FILE** infile) {
     char line[257];  // max of 256 char per line, 1 for null terminator
-    char* work_str;  // p* used to hold temporary split strings
+    char* buffer_1[80];  // p* used to hold temporary split strings
+    char* buffer_2[80];  // used to deal with name separation
     char*** master_storage = malloc(100000);  // value is arbitrary
     int i = 0, j = 0;   // Counters, max j expected to be constant for each line
 
@@ -42,35 +47,45 @@ int main(int argc, char** argv) {
     char last_name[] = "Fail";
     int user_number = -666;
 
-    for (; fgets(line, 256, infile) != NULL; i++) {
+    for (; fgets(line, 256, *infile) != NULL; i++) {
         // printf("%s\n", line);
         char* new_line = strchr(line, '\n');
         if (new_line)  
             *new_line = '\0';  // removes newline at end
-        work_str = strsplit(line, ",;:");  // splits string into tokens based on " ,;:/" char
+        buffer_1 = strsplit(line, ",;:");  // splits string into tokens based on " ,;:/" char
         master_storage[i] = malloc(256 * sizeof(char*));  // memory leaky but whatever
-        // if (NULL == work_str)
-            // printf("work_str is null\n");
+        // if (NULL == buffer_1)
+            // printf("buffer_1 is null\n");
 
-        for (j = 0; work_str != NULL; j++) {
-            char* new_str = malloc(strlen(work_str)+1);  // temporary hoding string
-            strcpy(new_str, work_str);
+        for (j = 0; buffer_1 != NULL; j++) {
+            char* new_str = malloc(strlen(buffer_1)+1);  // temporary hoding string
+            strcpy(new_str, buffer_1);
             master_storage[i][j] = new_str;
-            // printf("[Set %d, %d to %s]\n", i, j, work_str);
-            work_str = strsplit(NULL, ",;:");  // strsplit recycles memory, old overrides new
+            // printf("[Set %d, %d to %s]\n", i, j, buffer_1);
+            buffer_1 = strsplit(NULL, ",;:");  // strsplit recycles memory, old overrides new
         }
     }
 
+    // Printing code for storing master_storage in file
     for(int i_recall = 0; i_recall < i; i_recall++) {
         for(int j_recall = 0; j_recall < 10; j_recall++) {
-            // maximum number of 10 fields per line
+            // Maximum number of 10 fields per line
             printf("%d %d ", i_recall, j_recall);
             printf("%s\n", master_storage[i_recall][j_recall]);
         }
         printf("\n");
     }
+}
+
+
+int play() {
+    prinf("Points: %d  Play on", points); 
+
+int main(int argc, char** argv) {
+    char* user_list = argv[1];
+    FILE* infile = fopen(user_list, "r");
+    initialize(&infile);
 
     return 0;
 }
-
 
