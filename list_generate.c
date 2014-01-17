a70c5684f974de114a62a5b66d93179fe8fd65bc
LemonPi <johnson9510@hotmail.com>
2014-01-16 22:11:56 -0500

    Working edition with regularly sized storage

--- list_generate.c
+++ list_generate.c
@@ -1,6 +1,32 @@
+#define _GNU_SOURCE  // defines rawmemchr
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
+char* old_s;  // each time keep same pointer
+
+char* strsplit(char* s, const char* delim) {
+    char *piece;  // each time use new piece
+
+    if (s == NULL)
+        s = old_s;
+
+    if (*s == '\0') {
+        old_s = s;
+        return NULL;
+    }
+
+    piece = s;
+    s = strpbrk(piece, delim);  // finds location of next char in delim inside piece
+    if (s == NULL)
+        old_s = rawmemchr(piece, '\0');
+    else {
+        *s = '\0';
+        old_s = s + 1;
+    }
+    return piece;
+}
+
 
 int main(int argc, char** argv) {
     char* user_list = argv[1];
@@ -21,7 +47,7 @@ int main(int argc, char** argv) {
         char* new_line = strchr(line, '\n');
         if (new_line)  
             *new_line = '\0';  // removes newline at end
-        work_str = strtok(line, " ,;:/");  // splits string into tokens based on " ,;:/" char
+        work_str = strsplit(line, ",;:");  // splits string into tokens based on " ,;:/" char
         master_storage[i] = malloc(256 * sizeof(char*));  // memory leaky but whatever
         // if (NULL == work_str)
             // printf("work_str is null\n");
@@ -30,13 +56,14 @@ int main(int argc, char** argv) {
             char* new_str = malloc(strlen(work_str)+1);  // temporary hoding string
             strcpy(new_str, work_str);
             master_storage[i][j] = new_str;
-            printf("[Set %d, %d to %s]\n", i, j, work_str);
-            work_str = strtok(NULL, " ,;:/");  // strtok recycles memory, old overrides new
+            // printf("[Set %d, %d to %s]\n", i, j, work_str);
+            work_str = strsplit(NULL, ",;:");  // strsplit recycles memory, old overrides new
         }
     }
 
     for(int i_recall = 0; i_recall < i; i_recall++) {
-        for(int j_recall = 0; j_recall < j; j_recall++) {
+        for(int j_recall = 0; j_recall < 10; j_recall++) {
+            // maximum number of 10 fields per line
             printf("%d %d ", i_recall, j_recall);
             printf("%s\n", master_storage[i_recall][j_recall]);
         }
